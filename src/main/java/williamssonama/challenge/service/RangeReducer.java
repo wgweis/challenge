a73a0efc2c3f437c8e0f54ac28f22d3340beb94f
Warren George Weis <wgweis@sbcglobal.net>
2018-07-19 20:34:14 -0700

    Final checkin.

--- src/main/java/williamssonama/challenge/service/RangeReducer.java
+++ src/main/java/williamssonama/challenge/service/RangeReducer.java
@@ -19,6 +19,12 @@ public class RangeReducer {
      *  Things get tricky when we have more than two sets involved.  For that reason we have to remember what set we started from
      *  and ignore the starting boundary of any other sets encountered until we either encounter the ending boundary of the set
      *  we started from or our own ending boundary is less than ending boundary of a partially contained set.
+     *
+     *  This method also handles one outlier case:  the case where two intervals (sets) share a common border.  There may be more outliers
+     *  I have not considered.
+     *  The outlier case is dependent on Java 8 stream sorting not randomly shuffling the order of nodes with the same range value.  That is, putting
+     *  an end node before a start node if their values are the same.  Not necessarily the case if sort uses parallelization.
+     *
      * @param list
      * @return
      */
@@ -27,7 +33,9 @@ public class RangeReducer {
 
         NodeSearch searcher = new NodeSearch();
 
-        for (RangeTuple rt: list) {
+        for (int i = 0; i < list.size(); i++) {
+
+            RangeTuple rt = list.get(i);
             switch (rt.getRangeType()) {
                 case START: searcher.visitedStartNodes.put(rt.getStartIdentifier(), rt);
                             if (searcher.currentStart == null) {
@@ -42,9 +50,15 @@ public class RangeReducer {
                              if (rt.getStartIdentifier().equals(searcher.currentStart.getStartIdentifier())) {
                                  // we may have found the end of the union of our sets here.
                                  if (searcher.visitedStartNodes.isEmpty()) {
-                                     System.out.println("Range output [" + searcher.currentStart.getRangeValue() + ", " + rt.getRangeValue() + "]");
-                                     reducedRanges.add(new Integer[] {searcher.currentStart.getRangeValue(), rt.getRangeValue()});
-                                     searcher.currentStart = null;
+                                     if (doesNotShareBoundary(i, rt, list)) {
+                                         System.out.println("Range output [" + searcher.currentStart.getRangeValue() + ", " + rt.getRangeValue() + "]");
+                                         reducedRanges.add(new Integer[]{searcher.currentStart.getRangeValue(), rt.getRangeValue()});
+                                         searcher.currentStart = null;
+                                     } else if (i + 1 < list.size()){
+                                         // set the current start identifier to the next node so we can start looking for the end of that one.
+                                         searcher.currentStart.setStartIdentifier(list.get(i + 1).getStartIdentifier());
+
+                                     }
                                  } else {
                                      // continue on but with the current start value but the identifier of the next entry in the TreeMap
                                      Optional<RangeTuple> nextVisited
@@ -58,6 +72,15 @@ public class RangeReducer {
             }
         }
         return reducedRanges;
+    }
+
+    private boolean doesNotShareBoundary(int ndx, RangeTuple endNode, List<RangeTuple> list) {
+        int peakNdx = ndx + 1;
+        if (peakNdx < list.size()) {
+            return endNode.getRangeValue() < list.get(peakNdx).getRangeValue();
+        }
+        // case where we are at the end of the input.
+        return true;
     }
 
     private class NodeSearch {
