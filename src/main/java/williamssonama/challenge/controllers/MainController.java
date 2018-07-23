666209b775b8e531f7acac7aa1b8236fb7250372
Warren George Weis <wgweis@sbcglobal.net>
2018-07-23 09:25:35 -0700

    Modified Main Controller

--- src/main/java/williamssonama/challenge/controllers/MainController.java
+++ src/main/java/williamssonama/challenge/controllers/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 /**
- * Simply return the index hpage
+ * Simply return the index page
  */
 @Controller
 public class MainController {
